25c9004e9c1cd66d9326a286a5f449e36514471e
Simmi Mourya <simmim@seas.upenn.edu>
2020-05-10 07:04:37 -0400

    fixed IDF main

--- project_tfidf2/inverted_idx/src/inverted_idx/invertindex/InverseDocFrequency.java
+++ project_tfidf2/inverted_idx/src/inverted_idx/invertindex/InverseDocFrequency.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.regex.Pattern;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -22,159 +23,137 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.graalvm.compiler.word.Word;
-import java.text.Normalizer;
 
+import org.apache.hadoop.fs.ContentSummary;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 
 
-//class FormattedDateMatcher implements DateMatcher {
-// 
-//    private static Pattern DATE_PATTERN = Pattern.compile(
-//      "^\\d{4}-\\d{2}-\\d{2}$");
-// 
-//    @Override
-//    public boolean matches(String date) {
-//        return DATE_PATTERN.matcher(date).matches();
-//    }
-//}
+public class InverseDocFrequency extends Configured implements Tool {
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 
-public class TermFrequency extends Configured implements Tool {
-	
-    public static String stripAccents(String input){
-        return input == null ? null :
-                Normalizer.normalize(input, Normalizer.Form.NFD)
-                        .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-    }
-    
-
-	public static class Map extends
-			Mapper<Object, Text, Text, DoubleWritable> {
-
-//		private tokenfrequencyStruct docFrequency = new tokenfrequencyStruct();
-		private final static DoubleWritable one  = new DoubleWritable( 1);
-//		private Text token = new Text();
-		
-		List<String> stopwords = Arrays.asList("a", "about", "above", "above", "across", "after", "afterwards", 
-				"again", "against", "all", "almost", "alone", "along", "already", "also","although","always",
-				"am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone",
-				"anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because",
-				"become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", 
-				"beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call",
-				"can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail",
-				"do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", 
-				"elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything",
-				"everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five",
-				"for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further",
-				"get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter",
-				"hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however",
-				"hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself",
-				"keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me",
-				"meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much",
-				"must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine",
-				"no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off",
-				"often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
-				"ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather",
-				"re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should",
-				"show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something",
-				"sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that",
-				"the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby",
-				"therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those",
-				"though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", 
-				"toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us",
-				"very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever",
-				"where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", 
-				"which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", 
-				"within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the",
-				 "[", "]", "[ ]","a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s",
-				 "t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K"
-				,"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "1", "2", "3", "4", "5", "6",
-				"7", "8", "9", "0", "10"
-				);
-
-
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		private static final Pattern word_boundary = Pattern.compile("\\s*\\b\\s*"); // word  boundary start, end, midway
+		public void map(LongWritable LineNumber, Text line, Context context) throws IOException, InterruptedException {
 			
-			Text tokenFileName  = new Text();
-	        String token_fileNameString = "";
-
-//			String valString = value.toString().replaceAll("[^a-zA-Z0-9]+"," ");
-			String fileterd = value.toString().replaceAll("[^ \\nA-Za-zÀ-ÖØ-öø-ÿ]+"," ");
-			String stripAccentsFilter = stripAccents(fileterd);
+			String lineString = line.toString();
 			
+//			FileSplit InputfileSplit = (FileSplit) context.getInputSplit();
+//			String fileName = InputfileSplit.getPath().getName();
 			
+			String[] temp = lineString.split("#####");
+			String token = temp[0];
+	        String[] NormFrequency = temp[1].split("\t");
+	        String hashURL = NormFrequency[0];
+	        String freq = NormFrequency[1];
+	        
 			
-//			String dateString = value.
-//			PorterStemmer stemmer = new PorterStemmer();
-
-			StringTokenizer itr = new StringTokenizer(stripAccentsFilter);
-			FileSplit InputfileSplit = (FileSplit) context.getInputSplit();
-			String fileName = InputfileSplit.getPath().getName();
-			
-			while (itr.hasMoreTokens()) {
-				String word = itr.nextToken();
-				 if (word.isEmpty()) {
-		               continue;
-		            }
-				 
-				if (!stopwords.contains(word)) {
-//					docFrequency.set(fileName, 1);
-					
-					token_fileNameString = word.toLowerCase()+"#####" + fileName;
-		           	tokenFileName = new Text(token_fileNameString);
-		           	
-					context.write(tokenFileName, one);}
-			}
+	        for (String word  : word_boundary.split(lineString)) {
+	        	// for all words in the texts, start making key value pairs
+//	            if (word.isEmpty()) {
+//	               continue;
+//	            }	            
+	            context.write(new Text(token), new Text(hashURL+"=="+freq)); 
+	         }
 		}
 	}
 	
-	  	public static class Reduce extends Reducer<Text, DoubleWritable, Text, DoubleWritable > {
-		      @Override 
-		      public void reduce(Text word, Iterable<DoubleWritable> occurences, Context context) throws IOException, InterruptedException {
-		         double sum  = 0.0;  
-		         Text delimitedToken = new Text();
-//		         System.out.println("occurences " + occurences);
-		         
-		         for (DoubleWritable count : occurences) {
-		            sum  += count.get();
-		         }
-		         // Normalized term frequency: https://www.cl.cam.ac.uk/teaching/1314/InfoRtrv/lecture4.pdf see page 22
-		         sum = 1 + Math.log10(sum);
-//		         System.out.println("inside reduce    " + word + " " +  sum);
-		         
-		         String temp1 =  word.toString() + "#####" ;
-		         delimitedToken = new Text(temp1);
-		         
-		         context.write(delimitedToken, new DoubleWritable(sum));        
-		      }
-		   }
-	
+	  public static class Reduce extends Reducer<Text ,  Text ,  Text ,  DoubleWritable > {
+	      @Override 
+	      public void reduce( Text token,  Iterable<Text > hashURLFreq,  Context context)
+	         throws IOException,  InterruptedException {
+  
+	         double IDF = 0.0;
+	         int DF = 0;
+	         int TotalDocs = 0;
+	         Text delimitedToken = new Text();
 
+	         Configuration config= context.getConfiguration(); 
+	         TotalDocs = Integer.valueOf(config.get("length"));
+	         // stores the incoming key and it's value: hashURL==freq 
+	         //used to bookkeep the counts of tokens per document. -> hence to calculate DF
+	         HashMap<String, String> tokenOccurenceMap = new HashMap<String,String>();
 
-	public int run(String[] args) throws Exception {
-//		if (args.length != 2) {
-//			System.err.println("Usage: TermFrequency, Please pass correct input args lalalallal");
-//			System.exit(2);
+	         for (Text count  : hashURLFreq) {
+//	        	 System.out.println(token + " map out " + count);
+	        	 
+	        	 if(tokenOccurenceMap.containsKey(count.toString()) && (tokenOccurenceMap.containsValue(token.toString()))) {}
+	        	 else
+	        	 {
+	            	 tokenOccurenceMap.put(count.toString(), token.toString());
+	            	 DF++;
+	            	 }
+	            }
+	        
+//	         System.out.println("end one ----------------- DF " + DF);
+	         System.out.println(tokenOccurenceMap);
+	         
+	         for(String values: tokenOccurenceMap.keySet()){
+	        	 String[] parts = values.split("==");
+	             IDF = Math.log10(1 + (TotalDocs/DF));	             
+	         }
+	         
+	         String temp1 =  token.toString() + "#####" ;
+	         delimitedToken = new Text(temp1);		 
+	         context.write(delimitedToken, new DoubleWritable(IDF));
+	      
+	      }
+	   }
+	  
+//		public int run(String[] args) throws Exception {
+////			if (args.length != 2) {
+////				System.err.println("Usage: TermFrequency, Please pass correct input args lalalallal");
+////				System.exit(2);
+////			}
+//			
+//			Job job = Job.getInstance(getConf(), " TermFrequency ");
+//			
+//			job.setJarByClass(this.getClass());
+//			job.setMapperClass(Map.class);
+//			job.setReducerClass(Reduce.class);
+//			job.setOutputKeyClass(Text.class);
+//			job.setMapOutputValueClass(DoubleWritable.class);
+//			job.setOutputValueClass(DoubleWritable.class);
+//			
+//			FileInputFormat.addInputPath(job, new Path(args[0]));
+//			FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//			return (job.waitForCompletion(true) ? 0 : 1);
 //		}
 		
-		Job job = Job.getInstance(getConf(), " TermFrequency ");
-		
-		job.setJarByClass(this.getClass());
-		job.setMapperClass(Map.class);
-		job.setReducerClass(Reduce.class);
-		job.setOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(DoubleWritable.class);
-		job.setOutputValueClass(DoubleWritable.class);
-		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		return (job.waitForCompletion(true) ? 0 : 1);
-	}
+	  
+	   public int run( String[] args) throws  Exception {
+//		   FileSystem filesystem = FileSystem.get(getConf());
+//		   Path path = new Path(args[0]);
+//		   ContentSummary contentsummary = filesystem.getContentSummary(path);
+//		   long fileCount = contentsummary.getFileCount();
+		  long fileCount = 8306;
+		  Configuration config = new Configuration();
+//		  config.set("length", String.valueOf(fileCount));
+		  config.set("length", String.valueOf(fileCount));
+	      Job job  = Job.getInstance(config, " InverseDocFrequency ");
+	      job.setJarByClass(this.getClass());
+
+//	      FileInputFormat.addInputPath(job,  new Path(args[1]));
+//	      FileOutputFormat.setOutputPath(job,  new Path(args[ 2]));
+	      
+	      FileInputFormat.addInputPath(job,  new Path(args[0]));
+	      FileOutputFormat.setOutputPath(job,  new Path(args[1]));
+	      
+	      job.setMapperClass(Map.class);
+	      job.setReducerClass(Reduce.class);
+	      job.setOutputKeyClass(Text.class);
+	      job.setMapOutputValueClass(Text.class);
+	      job.setOutputValueClass(DoubleWritable.class);
+
+	      return job.waitForCompletion(true) ? 0 : 1;
+	   }
+	  
 	
-	public static void main(String[] args) throws Exception {
-		int res  = ToolRunner .run( new TermFrequency(), args);
-		System.exit(res);
-	}
-	
+	   
+	   public static void main( String[] args) throws  Exception {
+		   int res  = ToolRunner.run( new InverseDocFrequency(), args);
+	      System.exit(res);
+	   }
 	
 }
